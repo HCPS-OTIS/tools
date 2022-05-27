e5f635e60523559fed1e0fd059eb274481937cbb
connerdwelker <conner.welker@hcps.org>
2022-05-27 06:24:40 -0400

    added saved tabs

--- script.js
+++ script.js
@@ -14,13 +14,19 @@ for (let i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", copy);
 }
 
+try {
+    document.getElementById(location.hash.substring(1)).click();
+} catch (error) {
+    document.getElementById("cbs").click();
+}
+
 // if (document.body.animate) {
 //     document.querySelector('.copy-on-click').addEventListener('click', pop);
 // }
 
 function copy(e) {
     navigator.clipboard.writeText(e.target.innerText);
-    
+
     alert("Copied to clipboard!");
     // e.target.animate([
     //     { transform: 'scale(1)', opacity: 1 },
@@ -33,25 +39,20 @@ function copy(e) {
 }
 
 function openTab(evt, cityName) {
-    // Declare all variables
+    location.hash = evt.target.id;
+
     var i, tabcontent, tablinks;
 
-    // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
 
-    // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
 
-    // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
