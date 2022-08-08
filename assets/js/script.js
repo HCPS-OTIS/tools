bf55d34adc1f718992701d5d362ad04f633fd706
AidanRB <aidanrichbennett+github@gmail.com>
2022-08-08 10:22:23 -0400

    switch tooltips to js triggered elements allows for scrolling of navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
+// navigation tooltips
+function showTooltip(e) {
+    let tooltipText = e.target.getAttribute('data-tooltip')
+    if (tooltipText) {
+        let tooltip = document.createElement('div')
+        tooltip.innerHTML = tooltipText
+        tooltip.setAttribute('data-tooltip', tooltipText)
+
+        tooltip.classList = 'fading tooltip'
+        tooltip.style.top = e.srcElement.offsetTop + 26 + 'px'
+
+        document.body.appendChild(tooltip)
+        setTimeout(function () {
+            tooltip.classList.remove('fading')
+        }, 10)
+    }
+}
+
+function hideTooltip(e) {
+    let tooltips = document.querySelectorAll('.tooltip[data-tooltip="' + e.target.getAttribute('data-tooltip') + '"]')
+    tooltips.forEach(tooltip => {
+        tooltip.classList.add('fading')
+        setTimeout(function () {
+            tooltip.remove()
+        }, 300)
+    });
+}
+
 // show help if this help hasn't been seen before
 if (document.getElementById('help')) {
     // get data-help attribute from the .help
